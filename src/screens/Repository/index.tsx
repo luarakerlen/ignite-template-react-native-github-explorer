0458a4feb7f2cbe6f0a9676be87ee9d71e54a709
Luara Kerlen dos Santos Rangel <luarakerlen@hotmail.com>
2022-08-05 13:16:11 -0300

    complete repository code

--- src/screens/Repository/index.tsx
+++ src/screens/Repository/index.tsx
@@ -7,97 +7,89 @@ import { Background } from '../../components/Background';
 import { Card } from '../../components/Card';
 
 import {
-  Container,
-  RepoInfo,
-  OwnerAvatar,
-  TextGroup,
-  Description,
-  RepoStats,
-  Stars,
-  StarsCounter,
-  StarsText,
-  Forks,
-  ForksCounter,
-  ForksText,
-  OpenIssues,
-  OpenIssuesCounter,
-  OpenIssuesText,
-  IssuesList,
+	Container,
+	RepoInfo,
+	OwnerAvatar,
+	TextGroup,
+	Description,
+	RepoStats,
+	Stars,
+	StarsCounter,
+	StarsText,
+	Forks,
+	ForksCounter,
+	ForksText,
+	OpenIssues,
+	OpenIssuesCounter,
+	OpenIssuesText,
+	IssuesList,
 } from './styles';
 import { TitleAnimation } from './TitleAnimation';
 
 interface RepositoryParams {
-  repositoryId: number;
+	repositoryId: number;
 }
 
 export function Repository() {
-  const { params } = useRoute();
-  const { repositoryId } = params as RepositoryParams;
-  const { findRepositoryById } = useRepositories();
-  const repository = findRepositoryById(repositoryId);
+	const { params } = useRoute();
+	const { repositoryId } = params as RepositoryParams;
+	const { findRepositoryById } = useRepositories();
+	const repository = findRepositoryById(repositoryId);
 
-  function handleIssueNavigation(issueUrl: string) {
-    // TODO - use Linking to open issueUrl in a browser
-  }
+	function handleIssueNavigation(issueUrl: string) {
+		Linking.openURL(issueUrl);
+	}
 
-  return (
-    <Background>
-      <Container>
-        <RepoInfo>
-          {/* <OwnerAvatar source={{ uri:  }} /> */}
+	return (
+		<Background>
+			<Container>
+				<RepoInfo>
+					<OwnerAvatar source={{ uri: repository.owner.avatar_url }} />
 
-          <TextGroup>
-            <TitleAnimation>
-              {
-                // TODO - full name of the repository
-              }
-            </TitleAnimation>
+					<TextGroup>
+						<TitleAnimation>{repository.full_name}</TitleAnimation>
 
-            <Description numberOfLines={2}>{
-              //TODO - repository description
-            }</Description>
-          </TextGroup>
-        </RepoInfo>
+						<Description numberOfLines={2}>
+							{repository.description}
+						</Description>
+					</TextGroup>
+				</RepoInfo>
 
-        <RepoStats>
-          <Stars>
-            <StarsCounter>{
-              // TODO - repository stargazers count
-            }</StarsCounter>
-            <StarsText>Stars</StarsText>
-          </Stars>
+				<RepoStats>
+					<Stars>
+						<StarsCounter>{repository.stargazers_count}</StarsCounter>
+						<StarsText>Stars</StarsText>
+					</Stars>
 
-          <Forks>
-            <ForksCounter>{
-              // TODO - repository forks count
-            }</ForksCounter>
-            <ForksText>Forks</ForksText>
-          </Forks>
+					<Forks>
+						<ForksCounter>{repository.forks_count}</ForksCounter>
+						<ForksText>Forks</ForksText>
+					</Forks>
 
-          <OpenIssues>
-            <OpenIssuesCounter>{
-              // TODO - repository issues count
-            }</OpenIssuesCounter>
-            <OpenIssuesText>Issues{'\n'}Abertas</OpenIssuesText>
-          </OpenIssues>
-        </RepoStats>
+					<OpenIssues>
+						<OpenIssuesCounter>
+							{repository.open_issues_count}
+						</OpenIssuesCounter>
+						<OpenIssuesText>Issues{'\n'}Abertas</OpenIssuesText>
+					</OpenIssues>
+				</RepoStats>
 
-        <IssuesList
-          data={repository.issues}
-          keyExtractor={issue => String(issue.id)}
-          showsVerticalScrollIndicator={false}
-          renderItem={({ item: issue }) => (
-            <Card
-              data={{
-                id: issue.id,
-                title: issue.title,
-                subTitle: issue.user.login,
-              }}
-              // TODO - onPress prop calling 
-            />
-          )}
-        />
-      </Container>
-    </Background>
-  )
+				<IssuesList
+					data={repository.issues}
+					keyExtractor={(issue) => String(issue.id)}
+					showsVerticalScrollIndicator={false}
+					renderItem={({ item: issue }) => (
+						<Card
+							data={{
+								id: issue.id,
+								title: issue.title,
+								subTitle: issue.user.login,
+							}}
+							onPress={() => handleIssueNavigation(issue.html_url)}
+						/>
+					)}
+				/>
+			</Container>
+		</Background>
+	);
 }
